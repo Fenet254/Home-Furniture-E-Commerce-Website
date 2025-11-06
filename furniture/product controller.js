c86cefaedb47f94d957d6b08759653ca263bae26
Fenet Gizaw <simbani011@gmail.com>
2025-11-06 23:59:36 +0300

    Update product controller.js

--- furniture/product controller.js
+++ furniture/product controller.js
@@ -12,7 +12,8 @@ export const getProducts = async (req, res) => {
   }
   if (search) {
     const q = search.toLowerCase();
-    products = products.filter(p => p.name.toLowerCase().includes(q));
+    products = products.filter(p =>
+ p.name.toLowerCase().includes(q));
 
 
   }
